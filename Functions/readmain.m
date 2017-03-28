6775ad4d5241c249412df8e9c2ed8919bebf6f42
ChrisBail <baillard@uw.edu>
2017-03-28 13:00:00 -0700

    modification of the help description

--- Functions/readmain.m
+++ Functions/readmain.m
@@ -1,6 +1,12 @@
-%%% New version of readmain 
-
 function Main=readmain(file)
+% Function made to read the main configuration file used by PSPicker
+%
+% Input: file > main configuration file (type: str)
+% 
+% Output: Main > Structure containing all the parametesr defined in main config file (type: struct)
+% 
+% Example: Param=readmain('mainfile.txt')
+% 
 
 %%% Parameters
 
